47c1c4c6dec523023cb4b85b6d96aa631016133f
uzairwebdeveloper <aslam.uzair456@gmail.com>
2020-07-02 12:47:41 -0500

    added jquery slick.js and fontawesome cdn and added how it works section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/navbar';
 import Hero from './components/hero';
+import HowitWorks from './components/howitworks';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <HowitWorks />
       
     </div>
   );
---
+++ src/components/howitworks.js
@@ -0,0 +1,32 @@
+import React from 'react'
+const howitworks =() => {
+    return (
+       <section id="how-it-works">
+        <div class="container">
+            <h2>How It Works</h2>
+            <div class="flex">
+                <div>
+                    <span class="fas fa-home"></span>
+                    <h4>Find a Property.</h4>
+                    <p>Lorem ipsum dolor sit amet consectectur adipisicing elit.</p>
+                </div>
+
+                <div>
+                    <span class="fas fa-dollar-sign"></span>
+                    <h4>Buy a Property.</h4>
+                    <p>Lorem ipsum dolor sit amet consectectur adipisicing elit.</p>
+                </div>
+
+                <div>
+                    <span class="fas fa-chart-line"></span>
+                    <h4>Investing.</h4>
+                    <p>Lorem ipsum dolor sit amet consectectur adipisicing elit.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+            
+    
+    )
+}
+export default howitworks;
