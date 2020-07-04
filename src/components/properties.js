302fad0ce062a1098076e780194c12507256521b
uzairwebdeveloper <aslam.uzair456@gmail.com>
2020-07-04 17:05:58 -0500

    Hover effect added on the properties, and some final touches

--- src/components/properties.js
+++ src/components/properties.js
@@ -3,8 +3,9 @@ import React from "react";
 const properties = (props) => {
   return (
     <div id="properties">
-      <img src={props.imgsrc} alt={props.alt} />
       <div class="property-details">
+        <img src={props.imgsrc} alt={props.alt} />
+
         <p class="price">{props.price}</p>
         <span class="beds">{props.numofbeds}</span>
         <span class="baths">{props.numofbaths}</span>
